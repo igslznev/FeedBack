004d3ad9428357d0c0339fd33986d0a91e808e99
Igor <igor199408@gmail.com>
2016-06-02 00:02:25 +0300

    Added mapping for view of interview

--- src/main/java/com/common/controller/FeedbackController.java
+++ src/main/java/com/common/controller/FeedbackController.java
@@ -1,13 +1,18 @@
 package com.common.controller;
 
 import com.common.model.Interview;
+import com.common.service.InterviewService;
 import com.common.service.impl.InterviewServiceImpl;
+import org.aspectj.lang.annotation.Before;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import java.util.List;
 import java.util.Map;
 
@@ -15,25 +20,27 @@ import java.util.Map;
 public class FeedbackController {
 
     @Autowired
-    private InterviewServiceImpl interviewService;
+    private InterviewService interviewService;
 
-    @ModelAttribute("interviews")
+    /*@ModelAttribute("interviews")
     private List<Interview> getInterviews(){
         return interviewService.getAll();
-    }
+    }*/
 
     @RequestMapping("/")
     public String initializeForm(Map model)
     {
+        model.put("interviews", interviewService.getAll());
         return "feedback";
 
     }
 
-    @RequestMapping("/{idInterveiw}")
-    public String interviewInitalize(@RequestParam("idInterveiw") Long id)
+    @RequestMapping(value = "/interview")
+    public String interviewInitialize(@RequestParam(value = "id") Long id,Map model)
     {
+        model.put("interview", interviewService.getById(id));
 
-        return "feedback";
+        return "interview";
 
     }
 
